893ee341da4f2ba6b099bd055a2ced96a9ed172b
jay071234 <jay.s.dixit1234@gmail.com>
2026-02-25 15:46:35 +0530

    Added Runnable JAR file to execute easily on any device

--- src/test.java
+++ src/test.java
@@ -1,6 +1,6 @@
 public class test {
     public static void main(String[] args){
         String userHome = System.getProperty("User.home");
-        System.out.println(userHome);
+        System.out.println("hello world !!");
     }
 }
